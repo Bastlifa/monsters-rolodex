aa0afdbf45af0d18a9af0271fb4ae6bb3df7ca32
Eli Sacks <bastlifa@gmail.com>
2019-07-22 18:53:48 -0400

    Updates

---
+++ precache-manifest.371a9583e873c571b8e48e0323d3c8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d70ce303c0f86e87b03a7fe1751ba4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d78ef037d5aa87f7244",
+    "url": "/monsters-rolodex/static/css/main.98dac7ee.chunk.css"
+  },
+  {
+    "revision": "45c75d1e4fa2aa8ca950",
+    "url": "/monsters-rolodex/static/js/2.5541ef1f.chunk.js"
+  },
+  {
+    "revision": "4d78ef037d5aa87f7244",
+    "url": "/monsters-rolodex/static/js/main.bcb076ef.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
